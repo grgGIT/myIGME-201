89098b22c84771901e4b83f2a9dff0ef3c7bb002
Geoffrey Gracia <grg7576@rit.edu>
2023-09-01 20:27:50 -0400

    yayyyy first pe done

---
+++ Gracia_PE1/Gracia_PE1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gracia_PE1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jada Pinkett Smith");
+
+            //trying an int variable and doing a calculation
+            int num = 0;
+            for(int i = 0; i <10; i++)
+            {
+                num++;
+                Console.WriteLine(num);
+            }
+            //  I have coded in C# before (in IGME 105) so I understand some of the syntax differences {like ; being necessary at the end of a line} and that it's largely 
+            //similar concepts as javascript but C# is just more literal and strict, especially more than some other programs like p5.js 
+            
+        }
+    }
+}
