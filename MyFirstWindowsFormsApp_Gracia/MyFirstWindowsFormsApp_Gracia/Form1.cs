836f16d6827553c376f4022ac420cf86b8a9ca7c
Geoffrey Gracia <grg7576@rit.edu>
2023-09-11 00:24:11 -0400

    this is good question mark?????

--- MyFirstWindowsFormsApp_Gracia/MyFirstWindowsFormsApp_Gracia/Form1.cs
+++ MyFirstWindowsFormsApp_Gracia/MyFirstWindowsFormsApp_Gracia/Form1.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace MyFirstWindowsFormsApp_Gracia
 {
+    // Author: Geoff Gracia
+    // Purpose:Tutorial 0 learning how to create WindowsForms
+    // Restrictions: None
     internal class Form1
-    {
+    { // Method: Main
+      // Purpose: have available in the need
+      // Output: none?
+      // Restrictions: None
+        static void Main(string[] args)
+        {
+
+        }
     }
+   
+    
 }
