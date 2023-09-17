7b7494ccc84273efb54344699c6d1e79c093cbfb
Geoffrey Gracia <grg7576@rit.edu>
2023-09-17 14:01:23 -0400

    workinzz

---
+++ MyEditor_Gracia/MyEditor_Gracia/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace MyEditor_Gracia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BoldtoolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.italicstoolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.ColortoolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.FontToolStripButton4 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.sansSerifToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timesNewRomanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            this.fileToolStripMenuItem.ToolTipText = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.ToolTipText = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.ToolTipText = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.ToolTipText = "Save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.ToolTipText = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            this.editToolStripMenuItem.ToolTipText = "Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "&Copy";
+            this.copyToolStripMenuItem.ToolTipText = "Copy";
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cutToolStripMenuItem.Text = "&Cut";
+            this.cutToolStripMenuItem.ToolTipText = "Cut";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pasteToolStripMenuItem.Text = "&Paste";
+            this.pasteToolStripMenuItem.ToolTipText = "Paste";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
+            this.splitContainer1.SplitterDistance = 33;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 389);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BoldtoolStripButton1,
+            this.italicstoolStripButton2,
+            this.ColortoolStripButton3,
+            this.FontToolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BoldtoolStripButton1
+            // 
+            this.BoldtoolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BoldtoolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("BoldtoolStripButton1.Image")));
+            this.BoldtoolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BoldtoolStripButton1.Name = "BoldtoolStripButton1";
+            this.BoldtoolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.BoldtoolStripButton1.Text = "toolStripButton1";
+            this.BoldtoolStripButton1.ToolTipText = "Bold";
+            // 
+            // italicstoolStripButton2
+            // 
+            this.italicstoolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.italicstoolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("italicstoolStripButton2.Image")));
+            this.italicstoolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.italicstoolStripButton2.Name = "italicstoolStripButton2";
+            this.italicstoolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.italicstoolStripButton2.Text = "toolStripButton2";
+            this.italicstoolStripButton2.ToolTipText = "Italics";
+            // 
+            // ColortoolStripButton3
+            // 
+            this.ColortoolStripButton3.BackColor = System.Drawing.Color.Black;
+            this.ColortoolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ColortoolStripButton3.ForeColor = System.Drawing.Color.Magenta;
+            this.ColortoolStripButton3.ImageTransparentColor = System.Drawing.Color.Black;
+            this.ColortoolStripButton3.Name = "ColortoolStripButton3";
+            this.ColortoolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.ColortoolStripButton3.Text = "toolStripButton3";
+            this.ColortoolStripButton3.ToolTipText = "Color";
+            // 
+            // FontToolStripButton4
+            // 
+            this.FontToolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.FontToolStripButton4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sansSerifToolStripMenuItem,
+            this.timesNewRomanToolStripMenuItem});
+            this.FontToolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("FontToolStripButton4.Image")));
+            this.FontToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.FontToolStripButton4.Name = "FontToolStripButton4";
+            this.FontToolStripButton4.Size = new System.Drawing.Size(29, 22);
+            this.FontToolStripButton4.Text = "toolStripButton4";
+            this.FontToolStripButton4.ToolTipText = "Font";
+            // 
+            // sansSerifToolStripMenuItem
+            // 
+            this.sansSerifToolStripMenuItem.Name = "sansSerifToolStripMenuItem";
+            this.sansSerifToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sansSerifToolStripMenuItem.Text = "MS Sans Serif";
+            this.sansSerifToolStripMenuItem.ToolTipText = "MS Sans Serif";
+            this.sansSerifToolStripMenuItem.Click += new System.EventHandler(this.sansSerifToolStripMenuItem_Click);
+            // 
+            // timesNewRomanToolStripMenuItem
+            // 
+            this.timesNewRomanToolStripMenuItem.Name = "timesNewRomanToolStripMenuItem";
+            this.timesNewRomanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.timesNewRomanToolStripMenuItem.Text = "Times New Roman";
+            this.timesNewRomanToolStripMenuItem.ToolTipText = "Times New Roman";
+            this.timesNewRomanToolStripMenuItem.Click += new System.EventHandler(this.timesNewRomanToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BoldtoolStripButton1;
+        private System.Windows.Forms.ToolStripButton italicstoolStripButton2;
+        private System.Windows.Forms.ToolStripButton ColortoolStripButton3;
+        private System.Windows.Forms.ToolStripDropDownButton FontToolStripButton4;
+        private System.Windows.Forms.ToolStripMenuItem sansSerifToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timesNewRomanToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PE7_Gracia/PE7_Gracia/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PE7_Gracia
+{
+    internal class Program
+    {
+        // Author: Geoff Gracia
+        // Purpose: Creating a mad libs from external text files
+        // Restrictions: Using Text.IO
+        static void Main(string[] args)
+        {
+            // Method: Main
+            // Purpose:  where all of this code will occur  
+            // Restrictions: None
+
+            //beginning prompt 
+            Console.Write("Hello! Welcome to Mad Libs! Would you like to play?");
+            string userYN = Console.ReadLine().ToLower().Trim();
+
+            if(userYN == "yes")
+            {
+                Console.WriteLine("Great! Have fun!");
+                Console.WriteLine();
+            }
+            else if(userYN == "no")
+            {
+                Console.WriteLine("Hmmmm well this game is super fun so I am going to ask you AGAIN if you would like to play (hint: the answer is yes)");
+                Console.WriteLine();
+                Console.Write("Hello! Welcome to Mad Libs! Would you like to play?");
+                userYN = Console.ReadLine().ToLower().Trim();
+            }
+            else
+            {
+                Console.WriteLine("sorry can you answer again?");
+                Console.WriteLine();
+                Console.Write("Hello! Welcome to Mad Libs! Would you like to play?");
+                 userYN = Console.ReadLine().ToLower().Trim();
+            }
+
+            //creating a ref variable for the file 
+            //creating an array for each word
+            string  fileName = "C:\\Users\\Geoffrey Gracia\\source\\repos\\myIGME-201\\PE7_Gracia\\PE7_Gracia\\templates\\MadLibsTemplate.txt";
+            string[] madLibArr;
+
+            //prompt for name
+            Console.Write("What is your name? ");
+            string plyrName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("{0} is a great name!", plyrName);
+            Console.WriteLine();
+
+            /*
+           try
+            {
+                Console.WriteLine("Which Story would you like to choose 1 or N?");
+                string entry = Console.ReadLine();
+                if (entry == "1")
+                {
+
+                }
+                else if (entry == "N")
+                {
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            */
+
+         string savedTextFromFile  = File.ReadAllText(fileName);
+         madLibArr = savedTextFromFile.Split();
+
+
+            //for my own testing
+            foreach (string word in madLibArr)
+            {
+                
+                Console.WriteLine(word );
+                
+                
+            }
+         //string.replace 
+
+
+       
+
+            
+        }
+    }
+}
