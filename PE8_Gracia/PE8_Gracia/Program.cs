72afa05b0568592ab64e4b520d86f81eec9a6345
Geoffrey Gracia <grg7576@rit.edu>
2023-09-20 18:30:31 -0400

    wahoo

---
+++ PE8_Gracia/PE8_Gracia/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8_Gracia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        ///
+        //Q5 
+        ///
+
+        //z = 3y2 + 2x - 1
+        //	-1 <= x <= 1 in 0.1 increments
+        //	1 <= y <= 4 in 0.1 increments
+
+        //3d array
+        double[,,]  Arr3d;
+
+            /*
+            foreach (double z in Arr3d)
+            {
+                Arr3d[z] = 3y2 + 2x - 1
+            }
+            */
+
+            ///
+            //Q7
+            ///
+            Console.Write("Enter a random letter word of your choosing: ");
+            string readin = Console.ReadLine();
+
+
+            ///
+            //Q8
+            ///
+
+            Console.Write("Enter a random letter word of your choosing: ");
+            string readin = Console.ReadLine();
+
+            ///
+            //Q9
+            ///
+            string quotable = "";
+
+
+        }
+    }
+}
