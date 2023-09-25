00c1941a6fed93c8b2a52eda15aeeb8a24d1921f
Geoffrey Gracia <grg7576@rit.edu>
2023-09-24 21:59:06 -0400

    first weeee

---
+++ PE9_Gracia/PE9_Gracia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Gracia
+{
+    internal class Program
+    {
+        //author: Geoff Gracia
+        //purpose: to answer quesitons 2 and 3 in code
+        //restrictions: none
+        static void Main(string[] args)
+        {
+            //class: main
+            //purpose: where all of the code will be 
+            //restrictions: none
+
+            ///
+            //Q2
+            ///
+
+
+            ///
+            //Q3
+            ///
+        }
+    }
+}
