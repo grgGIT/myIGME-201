8838df7be735af220f62e1eb39f3912890eab59d
Geoffrey Gracia <grg7576@rit.edu>
2023-10-01 19:01:36 -0400

    initial

---
+++ Unit1Test_Gracia/Unit1Test_Gracia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Unit1Test_Gracia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a;
+            ulong b;
+            bool p;
+
+            float f;
+
+            p.ToString();
+
+            
+        }
+    }
+}
