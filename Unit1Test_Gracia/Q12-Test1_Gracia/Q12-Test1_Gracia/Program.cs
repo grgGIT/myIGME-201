0cd5d38fe72ab530d96d6c6232b3798f94d6dc78
Geoffrey Gracia <grg7576@rit.edu>
2023-10-02 08:15:13 -0400

    Q12 and Q 13 done here

--- Unit1Test_Gracia/Q12-Test1_Gracia/Q12-Test1_Gracia/Program.cs
+++ Unit1Test_Gracia/Q12-Test1_Gracia/Q12-Test1_Gracia/Program.cs
@@ -4,7 +4,59 @@ namespace Q12_Test1_Gracia
 {
     internal class Program
     {
-        static bool GiveRaise(string name, double salary)
+        //Q13 edits below
+
+        //struct and its pieces
+        struct Employee
+        {
+            public string sName;
+            public double dSalary;
+
+           
+        }
+      
+
+        static void Main(string[] args)
+        {
+            Employee myEmply = new Employee();
+           
+            //testing hint
+            Console.WriteLine("As a note for testing, YOUR NAME SHOULD BE 'Geoff' ");
+            Console.WriteLine();
+            //prompt
+            Console.Write("What is thy name (I'm looking for someone named Geoff)? : ");
+            //assign to struct property
+            myEmply.sName = Console.ReadLine();
+            Console.WriteLine();
+
+            //call method
+            GiveRaise(myEmply);
+
+           
+        }
+        //method to do the workinz stuff
+        static bool GiveRaise(Employee employee)
+        {
+            // if name is Geoff then salary raise
+            if (employee.sName == "Geoff")
+            {
+                employee.dSalary += 19000;
+                Console.WriteLine("Congratulations {0} on your new raise! Your new salary is {1}", employee.sName, employee.dSalary);
+                return true;
+            }
+            else
+            {
+                //otherwise exit game
+                Console.WriteLine("Ooo sorry no raise for you goodbye! Try again next time!");
+                Environment.Exit(0);
+                return false;
+            }
+
+
+            /* This is for Q12
+             
+
+              static bool GiveRaise(string name, double salary)
         {
             if (name == "Geoff")
             {
@@ -35,6 +87,9 @@ namespace Q12_Test1_Gracia
             GiveRaise(sName, dSalary);
 
            
+        }
+           
+             */
         }
     }
 }
