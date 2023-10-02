4deb00eaad67fd635632f913f8162de11d9d954a
Geoffrey Gracia <grg7576@rit.edu>
2023-10-02 07:17:24 -0400

    wee

---
+++ Unit1Test_Gracia/Q12-Test1_Gracia/Q12-Test1_Gracia/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Q12_Test1_Gracia
+{
+    internal class Program
+    {
+        static bool GiveRaise(string name, double salary)
+        {
+            if (name == "Geoff")
+            {
+                salary += 19000;
+                Console.WriteLine("Congratulations {0} on your new raise! Your new salary is {1}", name, salary);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Ooo sorry no raise for you goodbye! Try again next time!");
+                Environment.Exit(0);
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("As a note for testing, YOUR NAME SHOULD BE 'Geoff' ");
+            Console.WriteLine();
+            Console.Write("What is thy name (I'm looking for someone named Geoff)? : ");
+            sName = Console.ReadLine();
+            Console.WriteLine();
+
+            GiveRaise(sName, dSalary);
+
+           
+        }
+    }
+}
+  
--- Unit1Test_Gracia/Unit1Test_Gracia/Program.cs
+++ Unit1Test_Gracia/Unit1Test_Gracia/Program.cs
@@ -6,16 +6,58 @@ namespace Unit1Test_Gracia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
             int a;
             ulong b;
             bool p;
 
             float f;
 
-            p.ToString();
+           // p.ToString();
 
+
+            ///
+            // Q 9
+            ///
+
+            /*
+            double[][] dArray = new int[2][];
+            dArray[1] = new double[2];
+            dArray[2] = new double[1]
+
+            dArray[0][0] = 15;
+            dArray[1][2] = 5.67;
+            */
+
+
+            ///
+            //Q 10
+            ///
+          
             
+            /* byte byteVal;
+            short shortVal = -556;
+            byteVal = (byte)shortVal;
+            Console.WriteLine("byteVal = {0}", byteVal); */
+
+           /* byte byteVal;
+            short shortVal = -556;
+            byteVal = checked((byte)shortVal);
+            Console.WriteLine("byteVal = {0}", byteVal); */
+
+           /* byte byteVal;
+            short shortVal = -556;
+            byteVal = Convert.ToByte(shortVal);
+            Console.WriteLine("byteVal = {0}", byteVal);
+           */
+
+
+            ///
+            //Q 11
+            ///
+
+            //tried all names
         }
     }
 }
