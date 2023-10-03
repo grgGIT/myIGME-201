d6e83baf554b9387eb7381b0bd6b4e6616b12f55
Geoffrey Gracia <grg7576@rit.edu>
2023-10-03 13:34:19 -0400

    finished Q14

--- Unit1Test_Gracia/Q14_Test1_UT1_BugSquash_Gracia/Q14_Test1_UT1_BugSquash_Gracia/Program.cs
+++ Unit1Test_Gracia/Q14_Test1_UT1_BugSquash_Gracia/Q14_Test1_UT1_BugSquash_Gracia/Program.cs
@@ -8,59 +8,50 @@ namespace Q14_Test1_UT1_BugSquash_Gracia
 {
     internal class Program
     {
+        //Author: Geoff - from given code
+        // fix broken code
+        //limits: keep all code the same, just fix broken
+          
         static void Main(string[] args)
         {
             // Calculate x^y for y > 0 using a recursive function
-            static void Main(string[] args)
-            {
+
+            //removed duplicate Main 
+            
                 string sNumber;
                 int nX;
-                int nY
-                int nAnswer;
+            //added semicolon
+            int nY;
+            //chnaged nAnswer to a double
+                double nAnswer;
 
-                Console.WriteLine(This program calculates x ^ y.);
+            //added quotes
+                Console.WriteLine("This program calculates x ^ y.");
 
                 do
                 {
                     Console.Write("Enter a whole number for x: ");
-                    Console.ReadLine();
+                //made sNumber equal to readline
+                    sNumber =Console.ReadLine();
                 } while (!int.TryParse(sNumber, out nX));
 
                 do
                 {
                     Console.Write("Enter a positive whole number for y: ");
                     sNumber = Console.ReadLine();
-                } while (int.TryParse(sNumber, out nX));
+                //changed doWhile loop conditions for y (making it out nY not nX)
+                } while (int.TryParse(sNumber, out nY));
 
                 // compute the exponent of the number using a recursive function
-                nAnswer = Power(nX, nY);
+
+            //fixed Power to be Math.Pow
+                nAnswer = Math.Pow(nX, nY);
 
                 Console.WriteLine("{nX}^{nY} = {nAnswer}");
-            }
+            
 
 
-            int Power(int nBase, int nExponent)
-            {
-                int returnVal = 0;
-                int nextVal = 0;
-
-                // the base case for exponents is 0 (x^0 = 1)
-                if (nExponent == 0)
-                {
-                    // return the base case and do not recurse
-                    returnVal = 0;
-                }
-                else
-                {
-                    // compute the subsequent values using nExponent-1 to eventually reach the base case
-                    nextVal = Power(nBase, nExponent + 1);
-
-                    // multiply the base with all subsequent values
-                    returnVal = nBase * nextVal;
-                }
-
-                returnVal;
-            }
+            
 
             }
         }
