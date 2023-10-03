873284f13b17ac3e8e48812e0535f847f16aa4b1
Geoffrey Gracia <grg7576@rit.edu>
2023-10-03 16:20:48 -0400

    updated

--- Unit1Test_Gracia/Q4-Test1_Gracia/Q4-Test1_Gracia/Program.cs
+++ Unit1Test_Gracia/Q4-Test1_Gracia/Q4-Test1_Gracia/Program.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Q4_Test1_Gracia
 {
     internal class Program
     {
+        //Author: Geoff Gracia 
+        //purpose recreate 3 questions app
+        //limits: stay within what the app template is
+
+        //for the timer
+        private static Timer timer;
+        private static bool timeOver;
+
         static void Main(string[] args)
         {
+            //globals
             string playAgain;
+            //5 seconds
+            Program.timer = new Timer(5000.0);
+            //throws my method
+            Program.timer.Elapsed += new ElapsedEventHandler(Program.EndOfLine);
 
+            //prompt to start
             Console.Write("Wanna play this game? (yes/no): ");
             playAgain = Console.ReadLine().Trim().ToLower();
             if (playAgain == "no")
@@ -26,12 +41,13 @@ namespace Q4_Test1_Gracia
             }
             else
             {
-                Console.Write("Wanna play this game? ANSWER YES OR ELSE THE GAME ENDS (yes/no): ");
+                Console.Write("Wanna play this game? Hint: ANSWER YES OR ELSE THE GAME ENDS (yes/no): ");
                 playAgain = Console.ReadLine().Trim().ToLower();
             }
            
             Console.WriteLine();
 
+            //do-while for the whole program
             do 
             {
 
@@ -41,14 +57,19 @@ namespace Q4_Test1_Gracia
             string readEnter = Console.ReadLine();
             int parseEnter = int.Parse(readEnter);
 
-            //reading 1 2 and 3 
-            
-            if (readEnter == "1")
+                //reading 1 2 and 3 and then using it to pop up each individual prompt
+
+                //for time useage
+                timeOver = false;
+
+                if (readEnter == "1")
             {
                 Console.WriteLine("You have 5 seconds to answer the following question: ");
                 Console.Write("What is your favorite color? ");
+                timer.Start();
                 string choiceRead = Console.ReadLine().Trim().ToLower();
-                if (choiceRead == "black")
+                timer.Stop();
+                    if (choiceRead == "black")
                 {
                         Console.WriteLine("Well done!");
                 }
@@ -56,7 +77,7 @@ namespace Q4_Test1_Gracia
             
                 else
                 {
-                        Console.WriteLine("Sorry that's incorrect...");
+                        Console.WriteLine("Sorry you were incorrect...");
                         Console.WriteLine("The answer is: black, ");
                 }
 
@@ -66,15 +87,17 @@ namespace Q4_Test1_Gracia
             {
                 Console.WriteLine("You have 5 seconds to answer the following question: ");
                 Console.Write("What is the answer to life, the universe and everything?: ");
+                timer.Start();
                 string choiceRead = Console.ReadLine().Trim();
-                if (choiceRead == "42")
+                timer.Stop();
+              if (choiceRead == "42")
                 {
                     Console.WriteLine("Well Done!");
                 }
 
                 else
                 {
-                        Console.WriteLine("Sorry that's incorrect...");
+                        Console.WriteLine("Sorry you were incorrect...");
                         Console.WriteLine("The answer is: 42, ");
                 }
 
@@ -86,15 +109,16 @@ namespace Q4_Test1_Gracia
                 
                     Console.WriteLine("You have 5 seconds to answer the following question: ");
                     Console.Write("What is the airspeed velocity of an unladen swallow?: ");
-                    Console.WriteLine();
+                    timer.Start();
                     string choiceRead = Console.ReadLine().Trim().ToLower();
+                    timer.Stop();
                     if (choiceRead == "what do you mean? african or european swallow?")
                     {
                         Console.WriteLine("Well Done!");
                     }
                     else
                 {
-                    Console.WriteLine("Sorry that's incorrect...");
+                    Console.WriteLine("Sorry you were incorrect...");
                     Console.WriteLine("The answer is: What do you mean? African or European swallow?");
                 }
 
@@ -125,6 +149,14 @@ namespace Q4_Test1_Gracia
                 
         }while (playAgain == "yes");
 
+        }
+        //timer method 
+        private static void EndOfLine(object sender, ElapsedEventArgs e)
+        {
+            Program.timer.Stop();
+            Console.WriteLine("Time's up! Please press enter...");
+            Console.WriteLine();
+            Program.timeOver = true;
         }
     }
 }
