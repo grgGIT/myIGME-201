4a173e31c1e42e9d8dc4f87546428e0ad7950c16
Geoffrey Gracia <grg7576@rit.edu>
2023-10-19 23:29:01 -0400

    commit

---
+++ PE14_Gracia/PE14_Gracia/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE14_Gracia
+{
+    //Author: Geoff Gracia
+    //Purpose: answer to questions 1 thru 3
+    //Restrictions: none
+
+    //Question 1
+    public abstract class MyAbstractClass
+    {
+        private int myPrivateField;
+
+        // Public property to access the private field
+        public int MyProperty
+        {
+            get
+            {
+                return myPrivateField;
+            }
+            set
+            {
+                myPrivateField = value;
+            }
+        }
+    }
+
+    //
+    //
+    //
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
