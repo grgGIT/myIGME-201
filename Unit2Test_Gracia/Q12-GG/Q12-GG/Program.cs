efffcfa7e8a6c4154c79e236476460a849568e0b
Geoffrey Gracia <grg7576@rit.edu>
2023-10-29 23:05:02 -0400

    commit

---
+++ Unit2Test_Gracia/Q12-GG/Q12-GG/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Q12_GG
+{
+    internal class Program
+    {
+        public class Birthday
+        {
+            SortedList<string, DateTime> friendBirthdays = new SortedList<string, DateTime>();
+            //where string = friend's name and DateTime = their birthdate (use the format string "MM/dd/yyyy" in your output)
+            string friendDay = "04-21-1962";
+            DateTime parsedDate;
+
+			
+        
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
