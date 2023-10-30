66ecb990ee2504cdc650ac95fade36a85fa38db9
Geoffrey Gracia <grg7576@rit.edu>
2023-10-29 23:08:34 -0400

    commit

---
+++ Unit2Test_Gracia/Q13-GG/Q13-GG/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q13_GG
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            public int myInt2;
+            public List<string> myList;
+
+            public MyClass()
+            {
+                myInt2 = 42;
+                myList = new List<string>();
+                myList.Add("Item 1");
+                myList.Add("Item 2");
+            }
+
+            // Method to create a shallow copy
+            public MyClassCopy()
+            {
+                MyClass copy = new MyClass();
+                copy.myInt2 = this.myInt2;
+                copy.myList = this.myList; // Share the reference to the same list
+                return copy;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Unit2Test_Gracia/Q3-GG/Q3-GG/Program.cs
+++ Unit2Test_Gracia/Q3-GG/Q3-GG/Program.cs
@@ -34,46 +34,6 @@ namespace Q3_GG
 
             }
 
-        public class Birthday
-        {
-            SortedList<string, DateTime> friendBirthdays = new SortedList<string, DateTime>();
-            //where string = friend's name and DateTime = their birthdate (use the format string "MM/dd/yyyy" in your output)
-            string friendDay = "04-21-1962";
-            DateTime parsedDate;
-            foreach (var date in SortedList)
-        {
-            if (DateTime.TryParseExact(dateValue, pattern, null,
-                                      System.Globalization.DateTimeStyles.None, out parsedDate))
-                Console.WriteLine("Converted '{0}' to {1:d}.",
-                                  dateValue, parsedDate);
-            else
-                Console.WriteLine("Unable to convert '{0}' to a date and time.",
-                                  dateValue);
-        }
-    }
-
-        public class MyClass2
-        {
-            public int myInt2;
-            public List<string> myList;
-
-            public My()
-            {
-                myInt2 = 42;
-                myList = new List<string>();
-                myList.Add("Item 1");
-                myList.Add("Item 2");
-            }
-
-            // Method to create a shallow copy
-            public MyClassCopy()
-            {
-                MyClassCopy copy = new MyClass2();
-                copy.myInt2 = this.myInt2;
-                copy.myList = this.myList; // Share the reference to the same list
-                return copy;
-            }
-        }
 
         ///
         // Q3
