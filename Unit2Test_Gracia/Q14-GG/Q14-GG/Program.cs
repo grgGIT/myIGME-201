cc72b42e9ed800a8791ea421a2b73c5d12025a50
Geoffrey Gracia <grg7576@rit.edu>
2023-10-29 22:03:17 -0400

    commit

---
+++ Unit2Test_Gracia/Q14-GG/Q14-GG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q14_GG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
