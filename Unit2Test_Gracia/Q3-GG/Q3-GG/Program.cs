6dd7d8b12ff5ff8e54bc0691788e8b92a28a4eab
Geoffrey Gracia <grg7576@rit.edu>
2023-10-29 22:45:51 -0400

    commit

---
+++ Unit2Test_Gracia/Q3-GG/Q3-GG/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Q3_GG
+{
+    internal class Program
+    {
+
+
+
+
+
+        ///
+        // Question 11
+        ///
+        public class MyClass
+        {
+            public int myInt;
+
+            public MyClass(int nVal)
+            {
+                this.myInt += nVal;
+            }
+
+        }
+
+            public class MyDerivedClass : MyClass
+            {
+                public MyDerivedClass(int nVal) : base(nVal)
+                {
+                    this.myInt =(this.myInt + 2) *4;
+                }
+
+            }
+        
+
+        ///
+        // Q3
+        ///
+
+
+        static void Main(string[] args)
+        {
+            //for Q11
+            MyDerivedClass myObj = new MyDerivedClass(42);
+            Console.WriteLine(myObj.myInt);
+
+
+            //for Q3
+        }
+    }
+}
