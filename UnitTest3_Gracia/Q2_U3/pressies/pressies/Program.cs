8f25c637f6bc7cf452a4272ed278dbb6737cdca0
Geoffrey Gracia <89933856+grgGIT@users.noreply.github.com>
2023-12-07 14:37:50 -0500

    Fixed?

---
+++ UnitTest3_Gracia/Q2_U3/pressies/pressies/Program.cs
@@ -0,0 +1,47 @@
+<<<<<<< HEAD:UnitTest3_Gracia/Q2_U3/pressies/pressies/Program.cs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pressies
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pressies
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+>>>>>>> 7909980fc833ed03a3eadb1f9b8f0f5dee2ad468:UnitTest3_Gracia/Q3-U3_Gracia/Q2_U3/pressies/pressies/Program.cs
