e6a1a31d0e4e43c42d78c9a5af8f6c4732be4cb5
Geoff Gracia <grg7576@rit.edu>
2023-12-19 15:37:42 -0800

    I believe this should work

--- FINAL_EX_Gracia/Q9_Gracia/Q9_Gracia/Program.cs
+++ FINAL_EX_Gracia/Q9_Gracia/Q9_Gracia/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class Program
+{
+    public delegate double MyRounder(double d, int n);
+    static void Main(string[] args)
+    {
+        // create variable of delegate function type 
+        MyRounder myRounder;
+
+        // your code here
+        
+
+        // expressions for MyRounder delegate
+
+        myRounder = (d, n) => Math.Round(d, n); // Solution 1
+
+        myRounder = Math.Round; // Solution 2
+
+        myRounder = (double d, int n) => Math.Round(d, n); // Solution 3
+
+        myRounder = (d, n) => Math.Round(d, n); // Solution 4
+
+        myRounder = (double d, int n) => { return Math.Round(d, n); }; // Solution 5
+
+        myRounder = delegate (double d, int n) { return Math.Round(d, n); }; // Solution 6
+
+        myRounder = (d, n) => { return Math.Round(d, n); }; // Solution 7
+
+        myRounder = (double d, int n) => Math.Round(d, n); // Solution 8
+
+
+
+        
+    }
+}
