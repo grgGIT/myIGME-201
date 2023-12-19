bf42d9ead15efb4cf8b08489a485ca06c39a93b7
Geoff Gracia <grg7576@rit.edu>
2023-12-19 15:06:28 -0800

    I think this should be good

---
+++ FINAL_EX_Gracia/Q6_Gracia/Q6_Gracia/PlayerSettings.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace Q6_Gracia
+{
+    internal class PlayerSettings
+    {
+        public string PlayerName { get; set; }
+        public int Level { get; set; }
+        public int Hp { get; set; }
+        public string[] Inventory { get; set; }
+        public string LicenseKey { get; set; }
+
+        private static PlayerSettings instance;
+
+        private PlayerSettings() { }
+
+        public static PlayerSettings Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PlayerSettings();
+                }
+                return instance;
+            }
+        }
+
+        public void LoadSettings(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                instance = JsonConvert.DeserializeObject<PlayerSettings>(json);
+                Console.WriteLine("Settings loaded successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Settings file not found. Creating new settings.");
+                instance = new PlayerSettings();
+            }
+        }
+        public static PlayerSettings DefaultSettings()
+        {
+            return new PlayerSettings
+            {
+                PlayerName = "dschuh",
+                Level = 4,
+                Hp = 99,
+                Inventory = new string[]
+                {
+                "spear", "water bottle", "hammer", "sonic screwdriver", "cannonball",
+                "wood", "Scooby snack", "Hydra", "poisonous potato", "dead bush", "repair powder"
+                },
+                LicenseKey = "DFGU99-1454"
+            };
+        }
+        public void SaveSettings(string filePath)
+        {
+            string json = JsonConvert.SerializeObject(instance, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+            Console.WriteLine("Settings saved successfully.");
+        }
+    }
+}
--- FINAL_EX_Gracia/Q6_Gracia/Q6_Gracia/Program.cs
+++ FINAL_EX_Gracia/Q6_Gracia/Q6_Gracia/Program.cs
@@ -1,2 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Q6_Gracia;
+
+
+// Load or create player settings
+PlayerSettings playerSettings = PlayerSettings.Instance;
+playerSettings.LoadSettings("playerSettings.json");
+
+Console.WriteLine("Here are your default settings!");
+
+
+    playerSettings = PlayerSettings.DefaultSettings();
+    playerSettings.SaveSettings("playerSettings.json");
+
+Console.WriteLine($"Player Name: {playerSettings.PlayerName}");
+Console.WriteLine($"Level: {playerSettings.Level}");
+Console.WriteLine($"HP: {playerSettings.Hp}");
+Console.WriteLine("Inventory:");
+foreach (var item in playerSettings.Inventory)
+{
+    Console.WriteLine($"- {item}");
+}
+Console.WriteLine($"License Key: {playerSettings.LicenseKey}");
+
+
+Console.WriteLine("------------------------------------------------------------------------------------------------");
+
+
+Console.Write("Enter player name: ");
+playerSettings.PlayerName = Console.ReadLine();
+
+Console.Write("Enter player level: ");
+playerSettings.Level = int.Parse(Console.ReadLine());
+
+Console.Write("Enter player HP: ");
+playerSettings.Hp = int.Parse(Console.ReadLine());
+
+Console.Write("Enter items in inventory (comma-separated): ");
+string inventoryInput = Console.ReadLine();
+playerSettings.Inventory = inventoryInput.Split(',');
+
+Console.Write("Enter license key: ");
+playerSettings.LicenseKey = Console.ReadLine();
+
+playerSettings.SaveSettings("playerSettings.json");
+
+
+// Display current settings
+Console.WriteLine("Current Player Settings:");
+Console.WriteLine($"Player Name: {playerSettings.PlayerName}");
+Console.WriteLine($"Level: {playerSettings.Level}");
+Console.WriteLine($"HP: {playerSettings.Hp}");
+Console.WriteLine("Inventory:");
+foreach (var item in playerSettings.Inventory)
+{
+    Console.WriteLine($"- {item}");
+}
+Console.WriteLine($"License Key: {playerSettings.LicenseKey}");
+
+
+Console.WriteLine( "Thanks for viewing your stats with us (Statviewer Inc.!");
+// Save updated settings
+playerSettings.SaveSettings("playerSettings.json");
