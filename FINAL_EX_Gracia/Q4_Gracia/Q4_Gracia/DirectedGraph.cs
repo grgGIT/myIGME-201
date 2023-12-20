b308688868988a19479f6e8afe53745647b9bd39
Geoff Gracia <grg7576@rit.edu>
2023-12-19 18:12:42 -0800

    done?

---
+++ FINAL_EX_Gracia/Q4_Gracia/Q4_Gracia/DirectedGraph.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_Gracia
+{
+    internal class DirectedGraph
+    {
+        private int vertices;
+        private int[,] adjacencyMatrix;
+        private string[] labels = { "Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Cyan", "Gray" };
+
+        public DirectedGraph(int vertices)
+        {
+            this.vertices = vertices;
+            this.adjacencyMatrix = new int[vertices, vertices];
+        }
+
+        public void SetMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    adjacencyMatrix[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public void DFS(int startVertex)
+        {
+            bool[] visited = new bool[vertices];
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(startVertex);
+
+            while (stack.Count > 0)
+            {
+                int vertex = stack.Pop();
+
+                if (!visited[vertex])
+                {
+                    Console.Write($"{labels[vertex]} "); // Output the color
+                    visited[vertex] = true;
+
+                    for (int i = 0; i < vertices; i++)
+                    {
+                        if (adjacencyMatrix[vertex, i] == 1 && !visited[i])
+                        {
+                            stack.Push(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- FINAL_EX_Gracia/Q4_Gracia/Q4_Gracia/Program.cs
+++ FINAL_EX_Gracia/Q4_Gracia/Q4_Gracia/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Q4_Gracia;
+static void Main()
+{
+    DirectedGraph directedGraph = new DirectedGraph(8);
+
+    int[,] adjacencyMatrix = new int[,]
+    {
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 1, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 1, 1, 0},
+    };
+
+    directedGraph.SetMatrix(adjacencyMatrix);
+
+    Console.WriteLine("DFS starting from 'R' (Red):");
+    directedGraph.DFS(0); // 'R' is at index 0
+}
