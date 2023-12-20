bea8ca4d1a495ffcdc3a352365aac15dd4598bdb
Geoff Gracia <grg7576@rit.edu>
2023-12-19 18:00:39 -0800

    done?

---
+++ FINAL_EX_Gracia/Q3_Gracia/Q3_Gracia/DirectedGraph.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_Gracia
+{
+    public class DirectedGraph
+    {
+        private int vertices;
+        private int[,] adjacencyMatrix;
+        private char[] labels = { 'R', 'B', 'Y', 'G', 'P', 'O', 'C', 'K' };
+
+        public DirectedGraph(int vertices)
+        {
+            this.vertices = vertices;
+            this.adjacencyMatrix = new int[vertices, vertices];
+        }
+
+        public void SetMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    adjacencyMatrix[i, j] = matrix[i, j];
+                }
+            }
+        }
+
+        public void DisplayMatrix()
+        {
+            Console.WriteLine("Adjacency Matrix:");
+            Console.Write("    ");
+            for (int i = 0; i < vertices; i++)
+            {
+                Console.Write($"{labels[i]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < vertices; i++)
+            {
+                Console.Write($"{labels[i]} | ");
+                for (int j = 0; j < vertices; j++)
+                {
+                    Console.Write($"{adjacencyMatrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- FINAL_EX_Gracia/Q3_Gracia/Q3_Gracia/Program.cs
+++ FINAL_EX_Gracia/Q3_Gracia/Q3_Gracia/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Q3_Gracia;
+
+    static void Main()
+    {
+    DirectedGraph directedGraph = new DirectedGraph(8);
+
+        int[,] adjacencyMatrix = new int[,]
+        {
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 1, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 1, 1, 0},
+        };
+
+        directedGraph.SetMatrix(adjacencyMatrix);
+
+        // Displaying the adjacency matrix
+        directedGraph.DisplayMatrix();
+    }
